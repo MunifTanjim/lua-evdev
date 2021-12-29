c49bb49bb42a46b7e6daed0ee4f7de568373abf5
Munif Tanjim <hello@muniftanjim.dev>
2021-12-30 03:29:57 +0600

    chore: update scripts/lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env sh
 
+echo "[luacheck]"
+echo
+
 luacheck $@ .
+
+echo
+echo "[luarocks lint]"
+echo
+
+for rockspec in rockspecs/*; do
+  echo luarocks lint "${rockspec}"
+  luarocks lint "${rockspec}"
+done
