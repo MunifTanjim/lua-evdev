f615819288b5624307c9889a76b43a0f25d413d7
Munif Tanjim <hello@muniftanjim.dev>
2022-01-03 20:24:56 +0600

    chore: add lit metadata

---
+++ evdev/package.lua
@@ -0,0 +1,16 @@
+return {
+  name = "MunifTanjim/evdev",
+  version = "0.0.0",
+  description = "LuaJIT FFI Bindings for libevdev.",
+  tags = { "evdev", "libevdev", "ffi" },
+  license = "MIT",
+  author = {
+    name = "Munif Tanjim",
+    url = "https://muniftanjim.dev",
+  },
+  homepage = "https://github.com/MunifTanjim/lua-evdev",
+  dependencies = {},
+  files = {
+    "**.lua",
+  },
+}
